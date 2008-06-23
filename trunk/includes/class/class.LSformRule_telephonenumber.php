b92c7d281ae211994f1f9c2bcad16fc7c688ffc9
Benjamin Renard <brenard@easter-eggs.com>
2008-06-23 20:23:53 +0000

    - LSformRule_telephonenumber : Ajout d'une règle de validation d'un numéro   de téléphone (syntaxe française : 10 chiffres)

---
+++ trunk/includes/class/class.LSformRule_telephonenumber.php
@@ -0,0 +1,48 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2007 Easter-eggs
+ * http://ldapsaisie.labs.libre-entreprise.org
+ *
+ * Author: See AUTHORS file in top-level directory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+******************************************************************************/
+
+/**
+ * Règle de validation d'un numéro de téléphone.
+ *
+ * @author Benjamin Renard <brenard@easter-eggs.com>
+ */
+class LSformRule_telephonenumber extends LSformRule {
+ 
+ /**
+   * Vérification de la valeur.
+   *
+   * @param string $value Valeur à vérifier
+   * @param array $options Options de validation : 
+   *                                  - Check domain : $option['params']['checkDomain']
+   * @param object $formElement L'objet formElement attaché
+   *
+   * @return boolean true si la valeur est valide, false sinon
+   */
+  function validate($value,$option=array(),$formElement) {
+    $regex = '/^(01|02|03|04|05|06|08|09)[0-9]{8}$/';
+    $GLOBALS['LSsession'] -> loadLSclass('LSformRule_regex');
+    return LSformRule_regex :: validate($value,$regex,$formElement);
+  }
+
+}
+
+?>
