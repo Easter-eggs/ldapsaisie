55039206b70e5c654e521d4f7a936a4dd75ff00b
Benjamin Renard <brenard@easter-eggs.com>
2010-11-09 17:40:52 +0100

    Added script checkUpgrade.sh

---
+++ checkUpgrade.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ROOT_DIR=$( cd `dirname $0`; pwd )
+
+cd $ROOT_DIR
+
+git fetch -q
+
+CURRENT=`git show HEAD|head -n1`
+LASTEST=`git show FETCH_HEAD|head -n1`
+
+[ "$1" = "-d" ] && echo -e "Current : $CURRENT\nLastest : $LASTEST"
+
+if [ "$CURRENT" != "$LASTEST" ]
+then
+    echo "New update is available"
+    echo "======================="
+    echo "Current installation of LdapSaisie is from the $CURRENT."
+    echo "Changes have been made since."
+    echo
+    echo "The lastest commit is : "
+    echo 
+    git show FETCH_HEAD|cat
+    echo
+    echo "** /!\\ You have to run the script upgradeFromGit.sh to upgrade your installation. /!\\ **"
+fi
