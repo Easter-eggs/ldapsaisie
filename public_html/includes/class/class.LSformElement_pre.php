bd98151f8162105e973822d935e3a944454a524c
Benjamin Renard <brenard@easter-eggs.com>
2011-04-13 12:46:00 +0200

    Added LSformElement :: pre

---
+++ public_html/includes/class/class.LSformElement_pre.php
@@ -0,0 +1,50 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2007 Easter-eggs
+ * http://ldapsaisie.labs.libre-entreprise.org
+ *
+ * Author: See AUTHORS file in top-level directory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+******************************************************************************/
+
+LSsession :: loadLSclass('LSformElement_textarea');
+
+/**
+ * Element pre d'un formulaire pour LdapSaisie
+ *
+ * @author Benjamin Renard <brenard@easter-eggs.com>
+ */
+
+class LSformElement_pre extends LSformElement_textarea {
+
+  var $fieldTemplateExtraClass = 'LSformElement_pre';
+
+ /**
+  * Retourne les infos d'affichage de l'élément
+  * 
+  * Cette méthode retourne les informations d'affichage de l'élement
+  *
+  * @retval array
+  */
+  function getDisplay(){
+    $return = parent :: getDisplay();
+    LSsession :: addCssFile('LSformElement_pre.css');
+    return $return;
+  }
+
+}
+
+?>
