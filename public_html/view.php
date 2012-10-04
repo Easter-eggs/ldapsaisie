296e64696712f6174096b6e85d94ac0f20683d80
Benjamin Renard <brenard@easter-eggs.com>
2012-10-04 14:49:30 +0200

    CustomActions : Added hideLabel feature

--- public_html/view.php
+++ public_html/view.php
@@ -69,6 +69,7 @@ if(LSsession :: startLSsession()) {
                 if (LSsession :: canExecuteCustomAction($dn,$LSobject,$name)) {
                   $LSview_actions[] = array (
                     'label' => ((isset($config['label']))?__($config['label']):__($name)),
+                    'hideLabel' => ((isset($config['hideLabel']))?$config['hideLabel']:False),
                     'url' => 'custom_action.php?LSobject='.$LSobject.'&amp;dn='.urlencode($dn).'&amp;customAction='.$name,
                     'action' => ((isset($config['icon']))?$config['icon']:'generate'),
                     'class' => 'LScustomActions'.(($config['noConfirmation'])?' LScustomActions_noConfirmation':'')
