4bae44b259872ad38ae93a0f043442ac46e22033
Benjamin Renard <brenard@easter-eggs.com>
2014-11-20 00:45:26 +0100

    Add LSformElement :: supannEmpCorps

---
+++ public_html/includes/class/class.LSattr_html_supannEmpCorps.php
@@ -0,0 +1,34 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2007 Easter-eggs
+ * http://ldapsaisie.labs.libre-entreprise.org
+ *
+ * Author: See AUTHORS file in top-level directory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+******************************************************************************/
+
+/**
+ * Type d'attribut HTML supannEmpCorps
+ *
+ * @author Benjamin Renard <brenard@easter-eggs.com>
+ */
+class LSattr_html_supannEmpCorps extends LSattr_html {
+
+  var $LSformElement_type = 'supannEmpCorps';
+  
+}
+
+?>
