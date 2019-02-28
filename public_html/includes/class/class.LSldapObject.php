Easter-eggs/ldapsaisie

commit 274ccad0f2ba242a1005e810f17b96441fe22d77

LSldapObjet->validateAttrsData() : if LSform is specified, only check LSform's not freezed attributes

public_html/includes/class/class.LSldapObject.php
@@ -403,7 +403,9 @@ class LSldapObject {
     else {
       $LSform=false;
     }
-    foreach($this -> attrs as $attr) {
+    foreach($this -> attrs as $attr_name => $attr) {
+      if ($LSform && (!$LSform -> hasElement($attr_name) || $LSform -> isFreeze($attr_name)))
+        continue;
       $attr_values = $attr -> getValue();
       if (!$attr -> isValidate()) {
         if($attr -> isUpdate()) {

public_html/includes/class/class.LSform.php
@@ -500,6 +500,28 @@ class LSform {
   }
 
   /**
+   * Check if form has a specified element (by attr name)
+   *
+   * @param[in] $attr string The element/attribute name
+   *
+   * @retval boolean
+   **/
+  public function hasElement($name) {
+    return isset($this -> elements[$name]);
+  }
+
+  /**
+   * Check if a specified element (by attr name) is freezed
+   *
+   * @param[in] $attr string The element/attribute name
+   *
+   * @retval boolean
+   **/
+  public function isFreeze($name) {
+    return isset($this -> elements[$name]) && $this -> elements[$name] -> isFreeze($name);
+  }
+
+  /**
    * Ajoute une règle sur un élément du formulaire
    *
    * @author Benjamin Renard <brenard@easter-eggs.com>

commit 98ffc081c94aab22c6edf2fe1bb94f8d50891a45

LSldapObject : fix regression introduced by commit 274ccad0
  Handle all attributes in validateAttrsData() method but don't blame
  on missing required attribute on non-create form and if attibute is
  not present in form (or freezed).

public_html/includes/class/class.LSldapObject.php
@@ -404,8 +404,6 @@ class LSldapObject {
       $LSform=false;
     }
     foreach($this -> attrs as $attr_name => $attr) {
-      if ($LSform && (!$LSform -> hasElement($attr_name) || $LSform -> isFreeze($attr_name)))
-        continue;
       $attr_values = $attr -> getValue();
       if (!$attr -> isValidate()) {
         if($attr -> isUpdate()) {
@@ -427,6 +425,10 @@ class LSldapObject {
             }
           }
           else {
+            // Don't blame on non-create form for attributes not-present in form (or freezed)
+            if ($LSform && $idFrom != 'create' && (!$LSform -> hasElement($attr_name) || $LSform -> isFreeze($attr_name)))
+              continue;
+
             LSerror :: addErrorCode('LSattribute_06',$attr -> getLabel());
             $retval = false;
           }