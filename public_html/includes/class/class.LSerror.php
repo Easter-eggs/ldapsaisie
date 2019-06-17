fa96fc42ab4dd53c40831c604ed0b557cd8f9b42
Benjamin Renard <brenard@easter-eggs.com>
2019-06-17 13:34:02 +0200

    LSerror : fix error formatting when no error code is provided

--- public_html/includes/class/class.LSerror.php
+++ public_html/includes/class/class.LSerror.php
@@ -132,7 +132,7 @@ class LSerror {
   * @retvat string Le texte des erreurs
   */
   private static function getError($error) {
-    return "(Code ".htmlentities($error[0]).") ".htmlentities(getFData(self :: $_errorCodes[$error[0]]['msg'],$error[1]));
+    return ($error[0]?"(Code ".htmlentities($error[0]).") ":"").htmlentities(getFData(self :: $_errorCodes[$error[0]]['msg'],$error[1]));
   }
   
  /**
